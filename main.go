fdd14e74a219b125bf453b7b6a13aa0f7c3d71a1
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-30 14:54:17 +0100

    feat: simple web server in go with 2 extra routes

--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
   w.Write([]byte("Hello niggha"))
+
 }
 
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 
 func main() {
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", home)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-
-  log.Println("Starting server on :6000")
-  err := http.ListenAndServe(":6000", mux)
-  log.Fatal(err)
+	log.Println("Starting server on :6000")
+	err := http.ListenAndServe(":6000", mux)
+	log.Fatal(err)
 }
+
