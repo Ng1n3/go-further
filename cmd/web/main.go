01a2cac89d3bf3796eb511f8cda5e72dba5b39c0
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-31 00:51:46 +0100

    feat: listen and serve app on port the right way

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":5000", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	log.Println("Starting server on :5000")
-	err := http.ListenAndServe(":5000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
