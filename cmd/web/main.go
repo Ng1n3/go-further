5f83308075b402fc610fbb52f5826e84681f03c2
ng1n3 <code.with.muyiwa@gmail.com>
2025-01-31 00:40:01 +0100

    feat: serving static web file with css, js and tmpl

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,13 +5,15 @@ import (
 	"net/http"
 )
 
-
-
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
 	log.Println("Starting server on :5000")
 	err := http.ListenAndServe(":5000", mux)
