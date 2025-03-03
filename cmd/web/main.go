46c38a8b12bcf6c3d7d1774c9e793f5fe6baa63c
ng1n3 <code.with.muyiwa@gmail.com>
2025-03-04 00:51:47 +0100

    feat: create user model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,7 @@ type application struct {
 	snippets      *mysql.SnippetModel
 	session       *sessions.Session
 	templateCache map[string]*template.Template
+	users         *mysql.UserModel
 }
 
 func main() {
@@ -55,6 +56,7 @@ func main() {
 		snippets:      &mysql.SnippetModel{DB: db},
 		session:       session,
 		templateCache: templateCache,
+		users: &mysql.UserModel{DB: db},
 	}
 
 	// Initialize a tls.Config struct to hold the non-default TLS settings we want the server to usse
@@ -76,7 +78,7 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr)
 	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
-} 
+}
 
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -16,6 +16,14 @@ type Snippet struct {
 	Expires    time.Time
 }
 
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created_at     time.Time
+}
+
 func (s *Snippet) String() string {
 	return fmt.Sprintf("ID: %d\nTitle: %s\nContent: %s\nCreated: %s\nExpires: %s\n",
 		s.ID, s.Title, s.Content, s.Created_at.Format(time.RFC3339), s.Expires.Format(time.RFC3339))
